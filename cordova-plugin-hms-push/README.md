4e1e5c1c083332afcce7f16930fd6ef5c49c280b
Bengisu Ozkul (A) <bengisu.ozkul1@huawei.com>
2023-02-01 14:23:55 +0300

    UPDATE cordova-plugin-hms-push 6.7.0-300

---
+++ cordova-plugin-hms-push/README.md
@@ -0,0 +1,69 @@
+# Cordova HMS Push
+
+Cordova Push Kit is a messaging service provided for you. It establishes a messaging channel from
+the cloud to devices. By integrating Cordova Push Kit, you can send messages to your apps on users'
+devices in real time. This helps you maintain closer ties with users and increases user awareness of
+and engagement with your apps.
+
+This plugin enables communication between Huawei Push SDK and Cordova platform. It exposes all
+functionality provided by Huawei Push SDK.
+
+[> Learn More](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/introduction-0000001050136388?ha_source=hms1)
+
+## Installation
+
+### Cordova
+
+```bash
+$ cordova plugin add @hmscore/cordova-plugin-hms-push
+```
+
+### Ionic with Cordova Runtime
+
+```bash
+$ ionic cordova plugin add @hmscore/cordova-plugin-hms-push
+```
+
+### Ionic with Capacitor Runtime
+
+```bash
+$ npm install @hmscore/cordova-plugin-hms-push
+```
+
+### Ionic Native Wrappers
+
+```bash
+$ npm install @hmscore/ionic-native-hms-push
+```
+
+## Documentation
+
+- [Quick Start](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/prepare-dev-env-0000001050138343?ha_source=hms1)
+- [Reference](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-References/overview-0000001050136582?ha_source=hms1)
+
+### Additional Topics
+
+- [Enabling HUAWEI Push Service](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/config-agc-0000001050136396#EN-US_TOPIC_0000001050136396__section7787102592217?ha_source=hms1)
+- [Receiving Data Messages In App Killed State](https://developer.huawei.com/consumer/en/doc/development/HMS-Plugin-Guides/receiving-data-messages-at-app-killed-state-0000001058644809?ha_source=hms1)
+
+## Questions or Issues
+
+If you have questions about how to use HMS samples, try the following options:
+
+- [Stack Overflow](https://stackoverflow.com/questions/tagged/huawei-mobile-services) is the best
+  place for any programming questions. Be sure to tag your question
+  with **`huawei-mobile-services`**.
+- [GitHub](https://github.com/HMS-Core/hms-cordova-plugin) is the official repository for these
+  plugins, You can open an issue or submit your ideas.
+- [Huawei Developer Forum](https://forums.developer.huawei.com/forumPortal/en/home?fid=0101187876626530001?ha_source=hms1)
+  HMS Core Module is great for general questions, or seeking recommendations and opinions.
+- [Huawei Developer Docs](https://developer.huawei.com/consumer/en/doc/overview/HMS-Core-Plugin?ha_source=hms1)
+  is place to official documentation for all HMS Core Kits, you can find detailed documentations in
+  there.
+
+If you run into a bug in our samples, please submit an issue to
+the [GitHub repository](https://github.com/HMS-Core/hms-cordova-plugin).
+
+## License
+
+Huawei Push Kit Cordova Plugin is licensed under the [Apache 2.0 license](LICENSE).
---
+++ cordova-plugin-hms-push/www/HmsLocalNotification.js
@@ -0,0 +1,114 @@
+"use strict";
+/*
+    Copyright 2020-2023. Huawei Technologies Co., Ltd. All rights reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License")
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Visibility = exports.RepeatType = exports.Priority = exports.Importance = exports.Attr = exports.deleteChannel = exports.channelBlocked = exports.channelExists = exports.getChannels = exports.getScheduledNotifications = exports.getNotifications = exports.cancelNotificationsWithTag = exports.cancelNotificationsWithIdTag = exports.cancelNotificationsWithId = exports.cancelScheduledNotifications = exports.cancelNotifications = exports.cancelAllNotifications = exports.localNotificationSchedule = exports.localNotification = void 0;
+const utils_1 = require("./utils");
+const Interfaces_1 = require("./Interfaces");
+Object.defineProperty(exports, "Attr", { enumerable: true, get: function () { return Interfaces_1.Attr; } });
+Object.defineProperty(exports, "Importance", { enumerable: true, get: function () { return Interfaces_1.Importance; } });
+Object.defineProperty(exports, "Priority", { enumerable: true, get: function () { return Interfaces_1.Priority; } });
+Object.defineProperty(exports, "RepeatType", { enumerable: true, get: function () { return Interfaces_1.RepeatType; } });
+Object.defineProperty(exports, "Visibility", { enumerable: true, get: function () { return Interfaces_1.Visibility; } });
+function localNotification(localNotification) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "localNotification",
+        localNotification,
+    ]);
+}
+exports.localNotification = localNotification;
+function localNotificationSchedule(localNotificationSchedule) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "localNotificationSchedule",
+        localNotificationSchedule,
+    ]);
+}
+exports.localNotificationSchedule = localNotificationSchedule;
+function cancelAllNotifications() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelAllNotifications",
+    ]);
+}
+exports.cancelAllNotifications = cancelAllNotifications;
+function cancelNotifications() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelNotifications",
+    ]);
+}
+exports.cancelNotifications = cancelNotifications;
+function cancelScheduledNotifications() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelScheduledNotifications",
+    ]);
+}
+exports.cancelScheduledNotifications = cancelScheduledNotifications;
+function cancelNotificationsWithId(id) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelNotificationsWithId",
+        id,
+    ]);
+}
+exports.cancelNotificationsWithId = cancelNotificationsWithId;
+function cancelNotificationsWithIdTag(idTag) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelNotificationsWithIdTag",
+        idTag,
+    ]);
+}
+exports.cancelNotificationsWithIdTag = cancelNotificationsWithIdTag;
+function cancelNotificationsWithTag(tag) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "cancelNotificationsWithTag",
+        tag,
+    ]);
+}
+exports.cancelNotificationsWithTag = cancelNotificationsWithTag;
+function getNotifications() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", ["getNotifications"]);
+}
+exports.getNotifications = getNotifications;
+function getScheduledNotifications() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "getScheduledNotifications",
+    ]);
+}
+exports.getScheduledNotifications = getScheduledNotifications;
+function getChannels() {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", ["getChannels"]);
+}
+exports.getChannels = getChannels;
+function channelExists(channelId) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "channelExists",
+        channelId,
+    ]);
+}
+exports.channelExists = channelExists;
+function channelBlocked(channelId) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "channelBlocked",
+        channelId,
+    ]);
+}
+exports.channelBlocked = channelBlocked;
+function deleteChannel(channelId) {
+    return (0, utils_1.asyncExec)("HMSPush", "HmsLocalNotification", [
+        "deleteChannel",
+        channelId,
+    ]);
+}
+exports.deleteChannel = deleteChannel;
+//# sourceMappingURL=HmsLocalNotification.js.map
